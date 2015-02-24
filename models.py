fd5c6d836eb3c3beedbbf80adbc30a4977b15685
Stéphan Lascar <stephan.lascar@melme.net>
2015-02-24 06:59:50 +0100

    Ajout de divers catégories

--- models.py
+++ models.py
@@ -35,16 +35,16 @@ def get_category_for(transaction_type, label, amount):
             return u'Retraits, Chèques et Virements'
     else:
         p = [(re.compile('^.*FREE MOBILE|SEPA EDF|SEPA DOMEO.*$'), 'Abonnements'),
-             (re.compile('^.*MCDONALDS|CAFE BONHEUR|SARL LE LEPVRIER|COLUMBUS CAFE|LA VIGNERY|KFC|INTERMARCHE|AUCHAN VAL D\'EUR|PETIT CASINO|CARREFOURMARKET|E.LECLERC|PAX WHOLESOME|RARE BAR & GRILL|BUBBY S|APPLEBEES|BLEND HAMBURGER|GOURMET NEW YORK|STARBUCKS|LUIGI.S GRILL.*$'), 'Alimentation & Restaurant'),
-             (re.compile('^.*FNAC|NOCIBE|CELIO|TERRITOIRE REDSK|INTERSPORT|PHANTOM OF BROAD|THE LEGO STORE|MANGA MULTIMEDIA|ITUNES.COM|DECATHLON|YANKEES CLUBHOUS|LEVIS.*$'), 'Achats & Shopping'),
-             (re.compile('^.*EBOOKERS|USCUSTOMS|SEPA SERVICES MAGAZINES|TOP OF THE ROCK|AMER MUSEUM|AMNH-ADMISSIONS|MTA MVM.*$'), 'Loisirs & Sorties'),
-             (re.compile('^.*AVANSSUR|SEPA Bip Go|ADP ORLY|PERSONNEL 301214|CARREFMARKETDAC|AUCHAN CARBURANT|S L A.*$'), 'Auto & Transport'),
+             (re.compile('^.*MCDONALDS|CAFE BONHEUR|SARL LE LEPVRIER|COLUMBUS CAFE|LA VIGNERY|KFC|INTERMARCHE|AUCHAN VAL D\'EUR|PETIT CASINO|CARREFOURMARKET|E.LECLERC|PAX WHOLESOME|RARE BAR & GRILL|BUBBY S|APPLEBEES|BLEND HAMBURGER|GOURMET NEW YORK|STARBUCKS|LUIGI.S GRILL|LES DEUX FRERES|LE FORUM|WASABI.*$'), 'Alimentation & Restaurant'),
+             (re.compile('^.*FNAC|NOCIBE|CELIO|TERRITOIRE REDSK|INTERSPORT|PHANTOM OF BROAD|THE LEGO STORE|MANGA MULTIMEDIA|ITUNES.COM|DECATHLON|YANKEES CLUBHOUS|LEVIS|AUTREFOIS|DELICE DES PAPES|LA GDE RECRE|ALINEA|SEPA CREALFI SAS|H & M.*$|CASTORAMA'), 'Achats & Shopping'),
+             (re.compile('^.*EBOOKERS|USCUSTOMS|SEPA SERVICES MAGAZINES|TOP OF THE ROCK|AMER MUSEUM|AMNH-ADMISSIONS|MTA MVM|ROYAL KIDS|LA SERRE AU CROC|MAG NICOLAS.*$'), 'Loisirs & Sorties'),
+             (re.compile('^.*AVANSSUR|SEPA Bip Go|ADP ORLY|PERSONNEL|CARREFMARKETDAC|AUCHAN CARBURANT|S L A|RMG PARKING|STATION AVIA|RANDO PNEUS.*$'), 'Auto & Transport'),
              (re.compile('^.*ADVYS|Virement Internet.*$'), 'Salaires'),
              (re.compile('^.*SEPA PLAN|SEPA PayPal Europe S.a.r.l.*$'), 'Divers'),
              (re.compile(u'^.*Opération Carte|RETRAIT EXPRESS|RETRAIT|Retrait sur distributeurs.*$'), u'Retraits, Chèques et Virements'),
              (re.compile(u'^.*VERDONC|LASCAR.*$'), 'Virements internes'),
              (re.compile('^.*TCHIP|BODY MINUTE|YVAN ET ELODIE.*$'), u'Esthétique & Soins'),
-             (re.compile('^.*SANTE|SEPA GROUPAMA|RITE AID STORE|DR JUGIE RONGIE.*$'), u'Santé'),
+             (re.compile('^.*SANTE|SEPA GROUPAMA|RITE AID STORE|DR JUGIE RONGIE|PHIE DU MIDI|RAM PL PARIS|DOCTEUR GOUEZ|LABM ARMAINVIL.*$'), u'Santé'),
              (re.compile('^.*SEPA GMF|IMMOBILIER ECH.*$'), 'Logement'),
              (re.compile('^.*PRELEV./ACPTE FISCAL|SEPA DIRECTION GENERALE DE.*$'), u'Impôts & Taxes'),
              (re.compile('^.*Societe Gest Prest San|LCL CREDIT SATISFACTION.*$'), 'Remboursements'),
@@ -52,4 +52,4 @@ def get_category_for(transaction_type, label, amount):
         for pattern, category in p:
             if pattern.match(label):
                 return category
-    raise Exception('Unable to find category for %s' % label.encode('utf-8'))
+    raise Exception('Unable to find category for %s (%s €)' % (label.encode('utf-8'), amount))
